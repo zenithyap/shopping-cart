20d399c4b1c4863eee58313d26e0389e56dd4740
zenith <zenithyap88@gmail.com>
2025-08-07 11:25:30 +0800

    Link hompage button to shop

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,12 @@
-import styles from './home.module.css';
+import styles from './home.module.css'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <div className={styles.homeContainer}>
       <h1>Shoply</h1>
       <h2>The best shopping site ever created!</h2>
-      <button className={styles.btn}>Shop Now</button>
+      <Link to='shop' className={styles.btn}>Shop Now</Link>
     </div>
   );
 }
