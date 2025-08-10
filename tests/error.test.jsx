f498cf2d4a2fbba879fd64379f93c78471af8b34
zenith <zenithyap88@gmail.com>
2025-08-10 09:09:22 +0800

    Add error test

---
+++ tests/error.test.jsx
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Error from '../src/Components/Error/Error'
+
+describe('Error component', () => {
+    it('Renders correct heading', () => {
+        render(<Error />);
+        expect(screen.getByRole('heading', {name: 'Error 404: Nothing to be found here 🫠'}))
+    });
+});
