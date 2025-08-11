8f779b95418219b4c3be1e7455620132ac99b26e
zenith <zenithyap88@gmail.com>
2025-08-11 10:33:34 +0800

    Change button layout in cart

--- src/Components/CartRow/CartRow.jsx
+++ src/Components/CartRow/CartRow.jsx
@@ -3,17 +3,17 @@ import styles from './cartrow.module.css';
 export default function CartRow({ item, handleAddClick, handleRemoveClick }) {
   return (
     <div className={styles.row}>
-      <div className={styles.imageTitle}>
-        <div className={styles.imageContainer}>
-          <img src={item.image} alt={item.title} />
-        </div>
-        <p>{item.title}</p>
+      <div className={styles.imageContainer}>
+        <img src={item.image} alt={item.title} />
       </div>
+      <p>{item.title}</p>
       <p>Total Price: ${(item.price * item.quantity).toFixed(2)}</p>
       <div>
         <p>Quantity: {item.quantity}</p>
-        <button className={styles.leftButton} onClick={() => handleRemoveClick(item)}>-</button>
-        <button onClick={() => handleAddClick(item)}>+</button>
+        <div className={styles.buttonsContainer}>
+          <button onClick={() => handleRemoveClick(item)}>-</button>
+          <button onClick={() => handleAddClick(item)}>+</button>
+        </div>
       </div>
     </div>
   );
